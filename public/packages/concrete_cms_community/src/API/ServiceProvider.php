4e2cca8ff53a98b24d418f35ed0eec41c980a82e
Fabian Bitter <fabian@bitter.de>
2021-05-05 21:45:29 +0200

    Add api endpoint so receive badges from certifications module

--- public/packages/concrete_cms_community/src/API/ServiceProvider.php
+++ public/packages/concrete_cms_community/src/API/ServiceProvider.php
@@ -4,6 +4,7 @@ namespace PortlandLabs\Community\API;
 
 use Concrete\Core\Foundation\Service\Provider;
 use Concrete\Core\Routing\Router;
+use PortlandLabs\Community\API\V1\Badges;
 use PortlandLabs\Community\API\V1\Middleware\FractalNegotiatorMiddleware;
 use PortlandLabs\Community\API\V1\ShowcaseItems;
 use PortlandLabs\Community\API\V1\Teams;
@@ -26,6 +27,7 @@ class ServiceProvider extends Provider
                 $groupRouter->post('/showcase_items/update', [ShowcaseItems::class, 'update']);
                 $groupRouter->get('/showcase_items/delete', [ShowcaseItems::class, 'delete']);
                 $groupRouter->post('/teams/search', [Teams::class, 'search']);
+                $groupRouter->post('/badges/assign', [Badges::class, 'assign']);
             });
     }
 }
