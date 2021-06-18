a78491ebd87ac834ce11c01266a522e0fa1d162b
Andrew Embler <andrew@concrete5.org>
2021-06-18 10:31:10 -0700

    Fix new-484

--- public/packages/concrete_cms_hosting/controller.php
+++ public/packages/concrete_cms_hosting/controller.php
@@ -41,6 +41,10 @@ class Controller extends Package
     public function on_start()
     {
         // Register our service providers
+        /*
+         * Currently broken; something about enabling the ApiServiceProvider breaks the user avatar upload process
+         * :shrug: new-484. Re-enable this when it's time to fix this and enable the hosting.
         $this->app->make(ServiceProvider::class)->register();
+        */
     }
 }
