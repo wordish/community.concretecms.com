20e19358600ecd2546a558f7191a9124d421f8b1
Korvin Szanto <me@kor.vin>
2021-08-02 12:52:07 -0700

    Ignore E_USER_DEPRECATED errors

--- public/index.php
+++ public/index.php
@@ -5,4 +5,6 @@ if (isset($_SERVER['LAGOON_PROJECT'])) {
     putenv('CONCRETE5_ENV=lagoon');
 }
 
+const DEFAULT_ERROR_REPORTING = E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_USER_DEPRECATED;
+
 require 'concrete/dispatcher.php';
