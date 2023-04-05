ff37a3105040d03e65366c8342349852c9e5a7ae
Korvin Szanto <Korvinszanto@gmail.com>
2023-04-05 09:53:38 -0700

    Update to latest deploy setup

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-tempdir=/tmp/community/codedeployupload
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+source ../.codedeployvars
+tempdir=/tmp/codedeploy/codedeployupload/$jobid
 
 [[ -d $tempdir ]] && rm -r $tempdir
 mkdir -p $tempdir
@@ -9,13 +12,3 @@ mkdir -p $tempdir
 mkdir /tmp/$DEPLOYMENT_ID
 
 echo "export tempdir=\"$tempdir\"" > "/tmp/$DEPLOYMENT_ID/.cdvariables";
-
-if [ "$APPLICATION_NAME" == "community.stage.concretecms.com" ]
-then
-  echo "export projectdir=\"/home/forge/community.stage.concretecms.com\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-  echo "export deploydir=\"/home/forge/community.stage.concretecms.com/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-elif [ "$APPLICATION_NAME" == "community.concretecms.com" ]
-then
-  echo "export projectdir=\"/home/forge/community.concretecms.com\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-  echo "export deploydir=\"/home/forge/community.concretecms.com/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-fi
